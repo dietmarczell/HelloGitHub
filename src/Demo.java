8613afb4d6032407c2883e4c4413789e87fa8a7b
dietmarczell <czell.dietmar@gmx.at>
2015-10-10 15:18:51 +0200

    newest version with calculations

--- src/Demo.java
+++ src/Demo.java
@@ -11,8 +11,17 @@ public class Demo {
 		int numberLeafes = 0;
 		System.out.println("treeFront Boolean is \n <" + treeFront + "> \n");
 		System.out.println("My Leaf-Number is \n <" + numberLeafes + ">\n");
-		System.out.println(meinZeichen+"\n");
+		System.out.println(meinZeichen + "\n");
 		System.out.println(zeichenkette);
+
+		int a = 6;
+		int b = 6;
+		System.out.println(a + "+" + b + "=" + (a + b));
+		System.out.println(a + "-" + b + "=" + (a - b));
+		System.out.println(a + "*" + b + "=" + (a * b));
+		System.out.println(a + "/" + b + "=" + (a / b));
+		System.out.println(a + "%" + b + "=" + (a % b));
+
 	}
 
 }
