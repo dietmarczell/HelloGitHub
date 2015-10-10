b4369e67a82d8866374ef5c5c15eb0313ab32fa5
dietmarczell <czell.dietmar@gmx.at>
2015-10-10 15:07:02 +0200

    newest version after first programming day

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+import java.io.InputStream;
+
+public class Demo {
+
+	private static String zeichenkette = null;
+
+	public static void main(String[] args) {
+
+		boolean treeFront = true;
+		char meinZeichen = 'X';
+		int numberLeafes = 0;
+		System.out.println("treeFront Boolean is \n <" + treeFront + "> \n");
+		System.out.println("My Leaf-Number is \n <" + numberLeafes + ">");
+		System.out.println(meinZeichen);
+		
+		
+	}
+
+}
--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,7 +2,12 @@
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub, I'm developer 2!");
+		int in=1;
+		while ( in <= 10 )
+		{
+		System.out.println("Hello GitHub, I'm developer 2! Count is "+in);
+		in++;
+		}
 	}
 
 }
