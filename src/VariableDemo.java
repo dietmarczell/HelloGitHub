e4e91dd713dd77d2492ff351bb60400cc26f6340
dietmarczell <czell.dietmar@gmx.at>
2015-10-31 10:46:26 +0100

    new files

---
+++ src/VariableDemo.java
@@ -0,0 +1,10 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// vorgegebenen Namen ausgeben
+		String vorname = "Max";
+		String nachname = "Mustermann";
+		System.out.println("Der Name lautet: " + vorname + " " + nachname);
+	}
+}
