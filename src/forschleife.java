13db89739d0ba411510fdfbafa6a606a56e2325c
dietmarczell <czell.dietmar@gmx.at>
2015-11-13 14:31:21 +0100

    commit for-schleife

--- src/forschleife.java
+++ src/forschleife.java
@@ -2,10 +2,27 @@
 public class forschleife {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// 0-10
 		for (int i=0; i<=10; i++) {
 			System.out.println("Durchlauf "+i);
+		}
+		
+		// umgekehrt
+		for (int i=10; i>=0; i--) {
+			System.out.println("Durchlauf "+i);
+		}
+		
+		// jede 5te Zahl von 20-90 (incl.)
+		for (int i=20; i<=90; i+=5) {
+			System.out.println("Durchlauf "+i);
+		}
+		
+		// 1..100 nur die durch 3 teilbar sind
+		for (int i=1; i<=100; i++) {
+			if (i%3 == 0){
+				System.out.println("Durchlauf "+i);
+			}
 		}
 	}
 
