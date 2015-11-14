49d3ce9738b3c333806343dd2864cddc363f2419
dietmarczell <czell.dietmar@gmx.at>
2015-11-14 15:28:36 +0100

    cd

--- src/Car.java
+++ src/Car.java
@@ -6,5 +6,23 @@ public class Car {
 	public String Color;
 	public String Brand;
 
+	public void nextGear(){
+		CurrentGear++;
+	}
 	
+	public void setCurrentGear(int gear){
+		CurrentGear = gear;
+	}
+	
+	public int getCurrentGear(){
+		return CurrentGear;
+	}
+	
+	public void accelerate () {
+		CurrentSpeed += 10;
+	}
+	
+	public void accelerate (int speed) {
+		CurrentSpeed += speed;
+	}
 }
