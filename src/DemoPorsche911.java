17c3213de7113dbf54a83b18334dd5215ebdcda5
dietmarczell <czell.dietmar@gmx.at>
2015-11-14 14:37:22 +0100

    porsche demo

---
+++ src/DemoPorsche911.java
@@ -0,0 +1,17 @@
+
+public class DemoPorsche911 {
+
+	public static void main(String[] args) {
+		int n;
+		Car porsche911 = new Car();
+		porsche911.Color = "red";
+		porsche911.Brand = "Porsche";
+		porsche911.CurrentGear = 5;
+		porsche911.CurrentSpeed = 300;
+		
+		Waehrung Franken = new Waehrung();
+		Franken.Betrag = 5000;
+		Franken.Zeichen = "CHR";
+	}
+	
+}
