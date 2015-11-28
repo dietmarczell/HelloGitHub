4534a883ea39276269ef6b17cdbc377754434ef0
dietmarczell <czell.dietmar@gmx.at>
2015-11-28 11:09:06 +0100

    Update

--- src/maybe/DemoMaybe.java
+++ src/maybe/DemoMaybe.java
@@ -15,6 +15,14 @@ public class DemoMaybe {
 		Datum.MaybeCheck();
 		MayBe<Integer> Gehalt = new MayBe<Integer>(new Integer(1234), MayBe.ZugriffErlaubt);
 		Gehalt.MaybeCheck();
+		MayBe<Double> Zinssatz = new MayBe<Double>(new Double(1.125), MayBe.ZugriffErlaubt);
+		Zinssatz.MaybeCheck();
+		
+		// Klausur: do { Üben, probieren, Üben, Üben } while (schlecht);
+		// diesmal noch mit Moodle, exportieren per Git (Archiv) probieren für Hauptklausur
+		// Ohne Generics, sonst alles
+		// Arrays!, OP's, Classen, Konstruktor, Packages, schleifen, static, 
+		// 60min Dauer
 	}
 
 }
