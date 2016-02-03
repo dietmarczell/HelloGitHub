90a092e8988705574bdf5b3970ffe474beacd22c
dietmarczell <czell.dietmar@gmx.at>
2016-02-03 19:18:42 +0100

    checkin

--- src/lv11_tiere/Demo.java
+++ src/lv11_tiere/Demo.java
@@ -9,8 +9,14 @@ public class Demo {
 		Rectangle rect1 = new Rectangle(85, 10);
 		Rectangle rect2 = new Rectangle(120, 10);
 		Rectangle rect3 = new Rectangle(40, 10);
-		Circle krei1 = new Circle(200);
+		Circle krei1 = new Circle(50);
 		Circle krei2 = new Circle(100);
+		
+		System.out.println("rect1 umfang:"+rect1.getPerimeter()+" fläche:"+rect1.getArea());
+		System.out.println("rect2 umfang:"+rect2.getPerimeter()+" fläche:"+rect2.getArea());
+		System.out.println("rect3 umfang:"+rect3.getPerimeter()+" fläche:"+rect3.getArea());
+		System.out.println("krei1 umfang:"+krei1.getPerimeter()+" fläche:"+krei1.getArea());
+		System.out.println("krei2 umfang:"+krei2.getPerimeter()+" fläche:"+krei2.getArea());
 		
 		FigureManager figures = new FigureManager(new ArrayList<>());
 		
